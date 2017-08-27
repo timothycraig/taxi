06ae1cb0fab3bf3c1beb41b0c2a92b5d44c5f15a
Timothy Craig <ttcraig@Timothys-MacBook-Pro.local>
2017-08-26 21:54:34 -0400

    finish up create route

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -2,15 +2,16 @@
 const Driver = require('../models/Driver')
 
 module.exports = {
-  
+
   greeting (req, res) {
     res.send({ hi: 'there' })
   },
 
-  create (req, res) {
+  create (req, res, next) {
     const driverProps = req.body
 
     Driver.create(driverProps)
       .then(driver => res.send(driver))
+      .catch(next)
   }
 }
